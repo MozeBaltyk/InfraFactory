337fccc79d5a1928294af677f2be15ad505b4929
MozeBaltyk <morze.baltyk@proton.me>
2026-05-25 23:32:35 +0200

    feat(azure): wire Ansible integration matching OVH/Libvirt pattern
    - Create ansible.tf with check_cloudinit, reconcile_tls_san, and
      fetch_kubeconfig resources using shared Ansible playbooks
    - Remove raw-SSH fetch_kubeconfig from output.tf (superseded by
      Ansible-based approach in ansible.tf)
    - Reuse existing local.public_kube_api_endpoint from templates.tf
    
    Resolves: #68

--- providers/azure/output.tf
+++ providers/azure/output.tf
@@ -11,49 +11,6 @@ resource "local_file" "ansible_inventory" {
 }
 
 ###
-### Import Kubeconfig
-###
-resource "null_resource" "fetch_kubeconfig" {
-  count = contains(["k3s", "rke2"], var.cluster.cloud_init_selected) ? 1 : 0
-
-  depends_on = [
-    azurerm_linux_virtual_machine.vms
-  ]
-
-  triggers = {
-    path                     = local.env_path
-    ssh_endpoint             = azurerm_public_ip.vm-pip[local.first_master_name].ip_address
-    public_kube_api_endpoint = local.public_kube_api_endpoint
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-if [ "${var.cluster.cloud_init_selected}" = "rke2" ]; then
-  KUBE_CONF_PATH="/etc/rancher/rke2/rke2.yaml"
-elif [ "${var.cluster.cloud_init_selected}" = "k3s" ]; then
-  KUBE_CONF_PATH="/etc/rancher/k3s/k3s.yaml"
-else
-  echo "No kubeconfig path defined for cloud_init_selected=${var.cluster.cloud_init_selected}"
-  exit 0
-fi
-
-ssh -o StrictHostKeyChecking=no -i ${self.triggers.path}/.key.private \
-${var.cluster.username}@${self.triggers.ssh_endpoint} \
-"sudo cat $KUBE_CONF_PATH" | sed -E \
-  -e "s#server: https://127\\.0\\.0\\.1:[0-9]+#server: https://${self.triggers.public_kube_api_endpoint}:6443#" \
-> ${self.triggers.path}/kubeconfig
-EOT
-  }
-
-  # Cleanup on destroy
-  provisioner "local-exec" {
-    when    = destroy
-    command = "rm -f ${self.triggers.path}/kubeconfig"
-  }
-}
-
-
-###
 ### Display
 ###
 
